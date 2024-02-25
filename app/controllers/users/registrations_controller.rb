6283d7b2106582b5495fa88670dc936380679a2d
Ubuntu <ubuntu@ip-172-31-85-244.ec2.internal>
2024-02-25 17:47:34 +0000

    fixed registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  # before_action :check_registration_limit, only: [:new, :create]
   
   def create
-
     super  #call the parent
     @user = resource
     #make the first user the Manager. everyone else is Athlete
@@ -14,6 +14,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource.save
   end
   
+  protected
 
+  def check_registration_limit
+    if User.count == 5
+      if user_signed_in?
+        redirect_to projects_url
+      else
+        flash[:danger] = "Η εγγραφή δεν είναι δυνατή"
+        redirect_to root_path
+      end
+    end  
+  end
 
 end
